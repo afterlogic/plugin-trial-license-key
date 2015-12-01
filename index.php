8e1947dbe65a87bf8c16fc112413d703116473af
sasho4ek <a.ovcharov@gmail.com>
2015-12-01 16:09:30 +0300

    add index.php file

---
+++ index.php
@@ -0,0 +1,37 @@
+<?php
+
+/* -AFTERLOGIC LICENSE HEADER- */
+
+class_exists('CApi') or die();
+
+class CTrialLicenseKeyPlugin extends AApiPlugin
+{
+	/**
+	 * @param CApiPluginManager $oPluginManager
+	 */
+	public function __construct(CApiPluginManager $oPluginManager)
+	{
+		parent::__construct('1.0', $oPluginManager);
+
+		$this->AddHook('before-validate-license-key', 'BeforeValidateLicenseKey');
+	}
+
+	public function Init()
+	{
+		parent::Init();
+	}
+
+	/**
+	 * @param CAccount $oAccount
+	 */
+	public function BeforeValidateLicenseKey()
+	{
+		$oLicensingApi = CApi::Manager('licensing');
+		if ($oLicensingApi && 0 === strlen($oLicensingApi->GetLicenseKey()))
+		{
+			$oLicensingApi->UpdateLicenseKey($oLicensingApi->GetT());
+		}
+	}
+}
+
+return new CTrialLicenseKeyPlugin($this);
